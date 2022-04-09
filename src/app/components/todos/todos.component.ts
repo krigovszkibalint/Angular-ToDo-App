d97b997a09ccfd8b5d268511ea0b87f1730cb476
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-04-10 01:11:50 +0200

    Add Tasks

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,6 +9,8 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[];
 
+  inputTodo:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
@@ -32,6 +34,13 @@ export class TodosComponent implements OnInit {
   }
   deleteTodo (id:number) {
     this.todos = this.todos.filter((v, i) => i !== id);
+  }
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+    this.inputTodo = "";
   }
 
 }
