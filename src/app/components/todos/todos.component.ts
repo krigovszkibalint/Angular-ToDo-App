f6e85318c41a9e9e642fdd1e408ab97fc2440489
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-04-10 00:44:28 +0200

    Remove Tasks method

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -23,12 +23,15 @@ export class TodosComponent implements OnInit {
       }
     ]
   }
-  toggleDone(id: number) {
+  toggleDone(id:number) {
     this.todos.map((v, i) => {
       if (i == id) v.completed = !v.completed;
 
       return v;
     })
+  }
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
   }
 
 }
